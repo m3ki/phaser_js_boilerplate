b632f803f0d3310fd661ed2e3ae2f7cba16562b4
Igor Mekhtiev <imehdi@gmail.com>
2018-02-24 10:38:52 -0600

    wip

---
+++ source/Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("main-bower-files");
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      main: {
+        options: {
+          browserifyOptions: {
+            debug: true
+          },
+          transform: [
+            ["babelify", {
+              "stage": 1
+            }]
+          ]
+        },
+        src: "src/app.js",
+        dest: "scripts/app.js"
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          hostname: '*',
+          base: './deploy'
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: [
+          "node_modules/phaser/dist/phaser.min.js",
+
+          "src/lib/**/*.js",
+          "src/game/**/*.js"
+        ],
+        dest: 'deploy/js/<%= pkg.name %>.js'
+      }
+    },
+    watch: {
+      files: 'src/**/*.js',
+      tasks: ['concat']
+    },
+    open: {
+      dev: {
+        path: 'http://localhost:8080/index.html'
+      }
+    },
+    bower: {
+      flat: {
+        dest: 'scripts',
+        options: {
+          debugging: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['bower', 'concat', 'connect', 'open', 'watch']);
+
+}
